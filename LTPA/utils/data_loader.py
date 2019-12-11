e8d3b35bcf478b8154b64b5f6b56a8b8bba23595
TerenceLiu <terence@consiliumcrypto.ai>
2019-12-10 21:23:17 -0500

    Final touch for CIFAR10 + VGG19 Attention Network

--- LTPA/utils/data_loader.py
+++ LTPA/utils/data_loader.py
@@ -15,19 +15,20 @@ def get_data_loader(opt, im_size=32) -> torch.utils.data.DataLoader:
     transform_test = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+
     ])
     train_data = torchvision.datasets.CIFAR10(root=f'{ROOT_DIR}/data/CIFAR10',
-                                              train=True,
-                                              download=True,
-                                              transform=transform_train)
+                                            train=True,
+                                            download=True,
+                                            transform=transform_train)
+    test_data = torchvision.datasets.CIFAR10(root=f'{ROOT_DIR}/data/CIFAR10',
+                                            train=False,
+                                            download=True,
+                                            transform=transform_test)
     train_loader = torch.utils.data.DataLoader(train_data,
                                                batch_size=opt.batch_size,
                                                shuffle=True,
                                                num_workers=os.cpu_count())
-    test_data = torchvision.datasets.CIFAR10(root=f'{ROOT_DIR}/data/CIFAR10',
-                                             train=False,
-                                             download=True,
-                                             transform=transform_test)
     test_loader = torch.utils.data.DataLoader(test_data,
                                               batch_size=opt.batch_size,
                                               shuffle=False,
